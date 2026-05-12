8fa1e7d4cd571cdc87d86be1bf8805bbb019467b
Madeline Taveras <83628153+madelinetaveras@users.noreply.github.com>
2026-05-12 20:00:51 +0200

    Update event_validators.py

--- logic/event_validators.py
+++ logic/event_validators.py
@@ -1,13 +1,29 @@
 def validate_event(event):
+    """
+    Valida estructura mínima de un evento.
+
+    Inputs:
+        event: dict
+
+    Returns:
+        bool
+    """
+
     required_fields = [
         "Evento",
         "Zona",
         "Precio",
-        "Horario"
+        "Horario",
+        "Descripcion",
+        "Ambiente"
     ]
 
     for field in required_fields:
+
         if field not in event:
+            return False
+
+        if not event[field]:
             return False
 
     return True
